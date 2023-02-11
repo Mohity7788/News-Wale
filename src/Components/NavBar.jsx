dd828d79bb37f65ab92d1ded88b5b7e334bf1807
Mohit Kumar <mohit.kumar@mounttalent.com>
2023-02-12 00:42:02 +0530

    API integration

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 export default class NavBar extends Component {
+  articles = [];
+
   render() {
     return (
       <div>
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -2,22 +2,21 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+
     return (
-      <div>
-        <div className="card " style={{ width: "18rem" }}>
-          <img
-            src="https://www.rozgar.com/static/media/offered-you5.07308a5247c4e002024f.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
+      <div className="my-3 ">
+        <div className="card mb-3" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body text-centre">
+            <h5 className="card-title text-center">{title}</h5>
+            <p className="card-text text-centre">{description}</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary "
+            >
+              Read more
             </a>
           </div>
         </div>
