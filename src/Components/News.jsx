d0331382a84c8b6e547d1b0e7b4c5cb48c86111e
Mohit Kumar <mohit.kumar@mounttalent.com>
2023-02-18 00:05:03 +0530

    added loader and pagination button

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 class News extends Component {
   constructor() {
     super();
     this.state = {
       articles: [],
+      loading: false,
       page: 1,
     };
   }
   async componentDidMount() {
+    this.setState({ loading: true });
     let url =
       "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=17627e3ab39a43e9b045089e927af01f&pageSize=12";
     fetch(url)
@@ -19,69 +22,91 @@ class News extends Component {
         this.setState({
           articles: data.articles,
           totalArticles: data.totalResults,
+          loading: false,
         });
       });
   }
   handlePrevious = () => {
-    console.log("previous button clicked");
+    this.setState({ loading: true });
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=17627e3ab39a43e9b045089e927af01f&pageSize=12&page=${
       this.state.page - 1
     }`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        this.setState({ articles: data.articles, page: this.state.page - 1 });
+        this.setState({
+          articles: data.articles,
+          loading: false,
+          page: this.state.page - 1,
+        });
       });
   };
   handleNext = () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)) {
-    } else {
-      console.log("next button clicked");
-      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=17627e3ab39a43e9b045089e927af01f&pageSize=12&page=${
-        this.state.page + 1
-      }`;
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          this.setState({ articles: data.articles, page: this.state.page + 1 });
+    this.setState({ loading: true });
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=17627e3ab39a43e9b045089e927af01f&pageSize=12&page=${
+      this.state.page + 1
+    }`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          articles: data.articles,
+          loading: false,
+          page: this.state.page + 1,
         });
-    }
+      });
+    // if (this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)) {
+    // } else {
+    //   console.log("next button clicked");
+    //   let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=17627e3ab39a43e9b045089e927af01f&pageSize=12&page=${
+    //     this.state.page + 1
+    //   }`;
+    //   fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       this.setState({ articles: data.articles, page: this.state.page + 1 });
+    //     });
+    // }
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h2>News Wale - Top Headlines</h2>
-
+        <h2 className="text-center">News Wale - Top Headlines</h2>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((ele) => {
-            return (
-              <div className="col-md-4" key={ele.url}>
-                <NewsItem
-                  title={ele.title}
-                  description={ele.description}
-                  imageUrl={
-                    ele.urlToImage
-                      ? ele.urlToImage
-                      : "https://www.rozgar.com/static/media/courseSummary.d98ada59dc956b02ebf5.jpg"
-                  }
-                  newsUrl={ele.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((ele) => {
+              return (
+                <div className="col-md-4" key={ele.url}>
+                  <NewsItem
+                    title={ele.title}
+                    description={ele.description}
+                    imageUrl={
+                      ele.urlToImage
+                        ? ele.urlToImage
+                        : "https://www.rozgar.com/static/media/courseSummary.d98ada59dc956b02ebf5.jpg"
+                    }
+                    newsUrl={ele.url}
+                  />
+                </div>
+              );
+            })}
           <div className="container d-flex justify-content-between">
             <button
               disabled={this.state.page <= 1}
               type="button"
-              class="btn btn-dark"
+              class="btn btn-dark btn-previous"
               onClick={this.handlePrevious}
             >
               Previous Page
             </button>
             <button
               type="button"
-              class="btn btn-dark"
+              disabled={
+                this.state.page + 1 > Math.ceil(this.state.totalArticles / 20)
+              }
+              class="btn btn-dark btn-next"
               onClick={this.handleNext}
             >
               Next page
